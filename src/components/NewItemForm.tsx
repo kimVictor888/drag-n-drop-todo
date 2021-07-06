53b0e3e688a422a70832cd6eb281f64d1e8f0155
kimVictor888 <kim.victor.aleksandrovich@gmail.com>
2021-07-06 16:40:25 +0600

    feat: add newItemForm component

---
+++ src/components/NewItemForm.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { NewItemButton, NewItemFormContainer, NewItemInput } from "../styles";
+import { useFocus } from "../hooks/useFocus";
+
+type NewItemFormProps = {
+  onAdd(text: string): void;
+};
+
+export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  const inputRef = useFocus();
+
+  const handleAddText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onAdd(text);
+    }
+  };
+
+  return (
+    <NewItemFormContainer>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onKeyPress={handleAddText}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+    </NewItemFormContainer>
+  );
+};
